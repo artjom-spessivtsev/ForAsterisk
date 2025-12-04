480963519824ddcc8f9e15b31ea48a5f3340c05c
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2025-12-04 09:03:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ForAsterisk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Astetisk e *");
+
+            int nr = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nr; i++)
+            {
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
